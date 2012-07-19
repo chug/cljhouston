755e51f23ec1cbfbb565e075458233c4110091c2
Nelson Morris <nmorris@nelsonmorris.net>
2012-07-18 21:44:26 -0500

    Remove unused code

--- src/cljhouston/views/welcome.clj
+++ src/cljhouston/views/welcome.clj
@@ -15,37 +15,35 @@
 (defpage "/"
   []
   (html
-    (html5
-      [:head
-       [:title "CHUG: Clojure/Houston Users Group"]
-       (include-css "/bootstrap/css/bootstrap.min.css")
-       (include-js "/bootstrap/js/bootstrap.min.js")]
-      [:body 
-       [:div.container
-        [:div.row
-         [:div.span12
-          [:h1 "CHUG: Clojure/Houston Users Group"]
-          ]]
-        [:div.row
-         [:div.span12
-           [:h2 "Meeting Details"]
-           [:ul
-             [:li [:b "Time:"] " 7:00 pm on the fourth Thursday of the month"]
-             [:li [:b "Location:"] " INT, 2901 Wilcrest, Suite 300"]
-             [:li (link-to "http://groups.google.com/group/clj-houston" "Mailing List")]
-            ]]]
-        [:div.row
-         [:div.span12
-          [:h2 "Members"]
-          [:ul
-           (map #(vector :li %) members)]]]
-        [:div.row
-         [:div.span12
-          [:h2 "Next Meeting (5/24)"]
-          [:p "Work on the website."]]]
-        [:div.row
-         [:div.span12
-          [:h2 "Projects"]
-          [:p [:em "... you're looking at it ..."]]]]
-        ]])))
+   (html5
+    [:head
+     [:title "CHUG: Clojure/Houston Users Group"]
+     (include-css "/bootstrap/css/bootstrap.min.css")
+     (include-js "/bootstrap/js/bootstrap.min.js")]
+    [:body
+     [:div.container
+      [:div.row
+       [:div.span12
+        [:h1 "CHUG: Clojure/Houston Users Group"]]]
+      [:div.row
+       [:div.span12
+        [:h2 "Meeting Details"]
+        [:ul
+         [:li [:b "Time:"] " 7:00 pm on the fourth Thursday of the month"]
+         [:li [:b "Location:"] " INT, 2901 Wilcrest, Suite 300"]
+         [:li (link-to "http://groups.google.com/group/clj-houston"
+                       "Mailing List")]]]]
+      [:div.row
+       [:div.span12
+        [:h2 "Members"]
+        [:ul
+         (map #(vector :li %) members)]]]
+      [:div.row
+       [:div.span12
+        [:h2 "Next Meeting (5/24)"]
+        [:p "Work on the website."]]]
+      [:div.row
+       [:div.span12
+        [:h2 "Projects"]
+        [:p [:em "... you're looking at it ..."]]]]]])))
 
